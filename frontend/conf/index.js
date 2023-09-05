cde346be3b9418e343953f29d0e97ebf5a9384ae
krishnassss365@gmail.com <krishnassss365@gmail.com>
2023-09-06 01:52:22 +0530

    update3

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,10 +1,11 @@
 
-// const config = { backendEndpoint: "https://qtrip-dynamic-4495.onrender.com" };
 
-// export default config;
-
-const config = { backendEndpoint: "http://localhost:8082" };
+const config = { backendEndpoint: "https://qtrip-dynamic-4495.onrender.com" };
 
 export default config;
+
+// const config = { backendEndpoint: "http://localhost:8082" };
+
+// export default config;
 
 
